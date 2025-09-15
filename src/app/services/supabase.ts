72d3ecd43e0e4d4c89f11444a0d489fab368fbd0
CortesMalena <malenanahir16@gmail.com>
2025-09-15 17:13:24 -0300

    Arreglo el nombre del archivo con las variables de entorno

--- src/app/services/supabase.ts
+++ src/app/services/supabase.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/envitonment';
+import { environment } from '../../environments/environment';
 import { createClient, SupabaseClient, Session, User, AuthChangeEvent } from '@supabase/supabase-js';
 
 
